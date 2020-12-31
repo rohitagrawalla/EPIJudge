13dbcb4c605f1f589ae97d56bb1dd64f5894a0f3
Rohit Agrawalla <rohit.agrawalla@gmail.com>
2020-12-30 20:31:32 -0800

    closest int same weight solution

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -2,12 +2,19 @@ from test_framework import generic_test
 
 
 def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    NUM_BITS = 64
+    for i in range(NUM_BITS - 1):
+        if (x >> i) & 1 != (x >> (i + 1)) & 1:
+            x ^= (1 << i) | (1 << (i + 1))  # Swapsbit-i andbit-(i +1) return x
+            return x
 
+    return x
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('closest_int_same_weight.py',
-                                       'closest_int_same_weight.tsv',
-                                       closest_int_same_bit_count))
+        generic_test.generic_test_main(
+            "closest_int_same_weight.py",
+            "closest_int_same_weight.tsv",
+            closest_int_same_bit_count,
+        )
+    )
