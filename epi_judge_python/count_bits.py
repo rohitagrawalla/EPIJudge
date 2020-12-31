c916262c3874e017fad9528fd9a5bc83c1dacdb1
Rohit Agrawalla <rohit.agrawalla@gmail.com>
2020-12-30 17:18:01 -0800

    Adding solution for count_bits

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -1,12 +1,21 @@
 from test_framework import generic_test
 
 
+def count_bits_iterative(x: int) -> int:
+    c = 0
+    while x:
+        c += x & 1
+        x >>= 1
+
+    return c
+
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    c = 0
+    while x:
+        x &= x - 1
+        c += 1
 
+    return c
 
-if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
-                                       count_bits))
+if __name__ == "__main__":
+    exit(generic_test.generic_test_main("count_bits.py", "count_bits.tsv", count_bits))
